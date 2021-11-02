851d420f86f073c1625af468e53f8e9719d5d372
smillier <sylvain.millier@gmail.com>
2021-11-02 12:29:03 +0100

    Added mDNS responder (esp32.local)

--- Remplissage/Remplissage/src/main.cpp
+++ Remplissage/Remplissage/src/main.cpp
@@ -53,7 +53,115 @@ void IRAM_ATTR isr() {
 void setup()
 {
    
-   
+   //----------------------------------------------------Serial
+  Serial.begin(115200);
+  Serial.println("\n");
+  ///---------------------------------------------------Wifi
+  // put your setup code here, to run once:
+  Serial.print("\nStarting Async_AutoConnect_ESP32_minimal on " + String(ARDUINO_BOARD)); Serial.println(ESP_ASYNC_WIFIMANAGER_VERSION);
+#if ( USING_ESP32_S2 || USING_ESP32_C3 )
+  ESPAsync_WiFiManager ESPAsync_wifiManager(&server, NULL, "Bottle_Filler_Autoconnect");
+#else
+  ESPAsync_WiFiManager ESPAsync_wifiManager(&server, &dnsServer, "Bottle_Filler_Autoconnect");
+#endif  
+  //ESPAsync_wifiManager.resetSettings();   //reset saved settings
+  ESPAsync_wifiManager.setAPStaticIPConfig(IPAddress(192,168,4,1), IPAddress(192,168,4,1), IPAddress(255,255,255,0));
+  ESPAsync_wifiManager.autoConnect("Bottle_FillerAP");
+  if (WiFi.status() == WL_CONNECTED) { Serial.print(F("Connected. Local IP: ")); Serial.println(WiFi.localIP()); }
+  else { Serial.println(ESPAsync_wifiManager.getStatus(WiFi.status())); }
+ 
+ ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
+
+  //----------------------------------------------------GPIO
+  pinMode(led, OUTPUT);
+  pinMode(resetButton,INPUT_PULLDOWN);
+  pinMode(capteurTop, INPUT_PULLUP);
+
+  digitalWrite(led, HIGH);
+  pinMode(capteurPression, INPUT);
+
+  //----------------------------------------------------Interrupt
+  attachInterrupt(capteurTop, isr, FALLING);
+
+  //----------------------------------------------------SPIFFS
+  if (!SPIFFS.begin())
+  {
+    Serial.println("Erreur SPIFFS...");
+    //return;
+  }
+
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+
+  while (file)
+  {
+    Serial.print("File: ");
+    Serial.println(file.name());
+    file.close();
+    file = root.openNextFile();
+  }
+
+ 
+
+  //----------------------------------------------------WIFI
+	//WiFi.mode(WIFI_STA);
+  digitalWrite(led, LOW);
+  //WiFi.begin(ssid, password);
+  Serial.print("Tentative de connexion...");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\n");
+  Serial.println("Connexion etablie!");
+  Serial.print("Adresse IP: ");
+  Serial.println(WiFi.localIP());
+  //mDNS Responder
+  // Set up mDNS responder:
+    // - first argument is the domain name, in this example
+    //   the fully-qualified domain name is "esp32.local"
+    // - second argument is the IP address to advertise
+    //   we send our IP address on the WiFi network
+    if (!MDNS.begin("esp32")) {
+        Serial.println("Error setting up MDNS responder!");
+        while(1) {
+            delay(1000);
+        }
+    }
+    Serial.println("mDNS responder started");
+
+    Serial.println("TCP server started");
+
+    // Add service to MDNS-SD
+    MDNS.addService("http", "tcp", 80);
   //----------------------------------------------------SERVER
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/index.html", "text/html"); });
